951e0330ecd888cf8a353614198ea571c0a6ab11
legiac <legiac771216@gmail.com>
2021-11-30 17:20:58 +0700

    di lao dong

---
+++ tong2so.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ;
+ int main()
+ {
+     int a,b,tong;
+     cout<<"Nhap vao hai so a va b: ";
+     cin >> a >> b;
+     tong=a+b;
+     cout << tong;
+     return 0;
+
+ }
